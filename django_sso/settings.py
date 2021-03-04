45142124809f7b1555083e5437a00c0268f819c1
Rudy Artha <rudy.artha@bprlestari.com>
2021-03-04 16:34:48 +0800

    update log configuration

--- django_sso/settings.py
+++ django_sso/settings.py
@@ -196,7 +196,7 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             # 'handlers': ['console', 'file'],
             'handlers': ['console'],
             'propagate': False,
